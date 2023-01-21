b3765fca9d954db0ada4c8916fef38f95eeacd6a
Juho Taipale <krooh@hotmail.com>
2023-01-21 05:57:30 +0200

    comments added to untestable1

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,5 +1,7 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
-
+/*
+The rasult value depends on the day when this code is run so test with static value doesn't work.
+*/
 export function daysUntilChristmas() {
   const now = new Date();
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
