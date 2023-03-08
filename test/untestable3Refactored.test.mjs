5ef5693fbbdc8c21fe70c7b511a749d4af38d5d0
Juho Taipale <krooh@hotmail.com>
2023-03-08 14:56:25 +0200

    untestable3 test fix

--- test/untestable3Refactored.test.mjs
+++ test/untestable3Refactored.test.mjs
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { parsePeopleCsvData } from "../src/untestable3Refactored.mjs";
 
 describe("Untestable 3: CSV file parsing", () => {
-  it("todo", () => {
+  it("parse data", () => {
     expect(parsePeopleCsvData("Loid,Forger,,Male\nAnya,Forger,6,Female\nYor,Forger,27,Female"))
       .to.deep.equal(      [
         {
