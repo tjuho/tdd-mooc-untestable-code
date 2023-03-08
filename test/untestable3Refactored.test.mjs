5679fe914c5a677911efadb15c81a7e5e77d3d16
Juho Taipale <krooh@hotmail.com>
2023-03-08 05:54:57 +0200

    untestable3 test ready

--- test/untestable3Refactored.test.mjs
+++ test/untestable3Refactored.test.mjs
@@ -3,6 +3,25 @@ import { parsePeopleCsvData } from "../src/untestable3Refactored.mjs";
 
 describe("Untestable 3: CSV file parsing", () => {
   it("todo", () => {
-    expect(parsePeopleCsvData("Loid,Forger,,Male\nAnya,Forger,6,Female\nYor,Forger,27,Female")).to.deep.equal([]);
+    expect(parsePeopleCsvData("Loid,Forger,,Male\nAnya,Forger,6,Female\nYor,Forger,27,Female"))
+      .to.deep.equal(      [
+        {
+          "firstName": "Loid",
+          "gender": "m",
+          "lastName": "Forger"
+        },
+        {
+          "age": 6,
+          "firstName": "Anya",
+          "gender": "f",
+          "lastName": "Forger"
+        },
+        {
+          "age": 27,
+          "firstName": "Yor",
+          "gender": "f",
+          "lastName": "Forger"
+        }
+      ]);
   });
 });
