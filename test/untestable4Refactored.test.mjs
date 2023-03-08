9a9e4c50a636ab2eac4e1f7eb216886b9c3f0985
Juho Taipale <krooh@hotmail.com>
2023-03-08 14:57:47 +0200

    untestable4 test file added

---
+++ test/untestable4Refactored.test.mjs
@@ -0,0 +1,16 @@
+import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
+
+describe("Untestable 4: enterprise application", () => {
+  let service;
+  beforeEach(() => {
+    service = new PasswordService();
+  });
+
+  afterEach(() => {
+    PostgresUserDao.getInstance().close();
+  });
+
+  it("todo", async () => {
+    // TODO: write proper tests for both PasswordService and PostgresUserDao
+  });
+});
